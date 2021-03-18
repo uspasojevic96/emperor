3b93c41763fc99810012ec9e3717974f3908d3fa
Uros Spasojevic <uspasojevic96@gmail.com>
2021-03-18 20:51:50 +0100

    [deps] final fix

---
+++ src/discord/User.h
@@ -0,0 +1,28 @@
+namespace discord {
+    enum class UserFlag {
+        None = 0,
+        DiscordEmployee = 1 << 0,
+        PartneredServerOwner = 1 << 1,
+        HypeSquadEvents = 1 << 2,
+        BugHunterLevel1 = 1 << 3,
+        HouseBravery = 1 << 6,
+        HouseBrilliance = 1 << 7,
+        HouseBalance = 1 << 8,
+        EarlySupporter = 1 << 9,
+        TeamUser = 1 << 10,
+        System = 1 << 12,
+        BugHunterLevel2 = 1 << 14,
+        VerifiedBot = 1 << 16,
+        EarlyVerifiedBotDeveloper = 1 << 17
+    };
+
+    enum class PremiumType {
+        None = 0,
+        NitroClassic = 1,
+        Nitro = 2
+    };
+
+    class User {
+        std
+    }
+}
